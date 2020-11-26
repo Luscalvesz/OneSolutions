a70f2a4e205eeca475ceab66f1913dd3a52ef15e
unknown <joao.rosa@bandtec.com.br>
2020-11-26 16:07:03 -0300

    Conetado no banco Azure

--- site/public/apiConectBanco/app/database.js
+++ site/public/apiConectBanco/app/database.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
     port     : '3306',
-    user     : 'Usuario',//usuario local
-    password : 'urubu100',//senha usuario local
+    user     : 'One',//usuario local
+    password : 'onesolutions',//senha usuario local
     database : 'one_solutions'
 });
 
